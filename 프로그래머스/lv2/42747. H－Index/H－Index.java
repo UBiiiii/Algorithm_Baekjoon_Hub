59c9eac43a70106727447880426e384fde9eb823
UBiiiii <96060708+UBiiiii@users.noreply.github.com>
2023-08-16 10:47:55 +0900

    [level 2] Title: H-Index, Time: 0.68 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int n = -1;
+        Arrays.sort(citations);
+        while(++n < citations.length && citations[n] < citations.length - n);
+        return citations.length - n;
+    }
+}
