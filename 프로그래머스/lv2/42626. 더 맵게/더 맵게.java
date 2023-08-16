655d22931c329a110c9fadf134a6d6ddc1b38002
UBiiiii <96060708+UBiiiii@users.noreply.github.com>
2023-08-16 10:46:07 +0900

    [level 2] Title: 더 맵게, Time: 1652.18 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Long>queue = new PriorityQueue<>();
+        for(int i : scoville) queue.offer(Long.valueOf(i));
+        while(queue.peek()<K) {
+            if(queue.size()==1) return -1;
+            queue.offer(queue.poll() + queue.poll()*2);
+            answer++;
+        }
+        return answer;
+    }
+}
