878d8f6f48ef02e03be38e57463228097c94c4e1
UBiiiii <96060708+UBiiiii@users.noreply.github.com>
2023-08-16 10:48:22 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        for(int i=1; i<=yellow; i++) 
+            if(yellow%i==0 && (i+2)*(yellow/i + 2)-yellow == brown)
+                return new int[] {yellow/i + 2, i + 2};
+        return new int[] {};
+    }
+}
