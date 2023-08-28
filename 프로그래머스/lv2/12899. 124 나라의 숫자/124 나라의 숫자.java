d12b11471412a608e365efe4a551f78591b259c2
UBiiiii <96060708+UBiiiii@users.noreply.github.com>
2023-08-28 22:50:00 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.04 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        while(n > 0) {
+            int num = n/3;
+            switch(n - num*3) {
+                case 0:
+                    num -= 1;
+                    sb.append(4);
+                    break;
+                default:
+                    sb.append(n-num*3);
+            }
+            n = num;
+        }
+
+        return sb.reverse().toString();
+    }
+}
