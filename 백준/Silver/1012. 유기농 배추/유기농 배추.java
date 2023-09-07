b89cb7440c8d0ee11a55d6b90b18aefe7f3428ee
UBiiiii <96060708+UBiiiii@users.noreply.github.com>
2023-09-07 16:37:21 +0900

    [Silver II] Title: 유기농 배추, Time: 164 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,67 +1,65 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static int[][] map;
+    private static int n, m, nx, ny;
+    private static int[] dx = {0, 1, 0, -1}, dy = {1, 0, -1 ,0};
+    private static boolean[][] map, isChecked;
+    private static Queue<int[]> q;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t, k, x, y, cnt;
+        t = Integer.parseInt(br.readLine());
+        for(int cn=0;cn<t;cn++) {
+            q = new LinkedList<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+            cnt = 0;
+            map = new boolean[m][n];
+            isChecked = new boolean[m][n];
+            for(int i=0;i<k;i++) {
+                st = new StringTokenizer(br.readLine());
+                x = Integer.parseInt(st.nextToken());
+                y = Integer.parseInt(st.nextToken());
+                map[x][y] = true;
+            }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int c = Integer.parseInt(sc.nextLine());
-		for(int i=0;i<c;i++) {
-			int answer = 0;
-			String str = sc.nextLine();
-			String[] mapData = str.split(" ");
-			int bx = Integer.parseInt(mapData[0]);
-			int by = Integer.parseInt(mapData[1]);
-			int b = Integer.parseInt(mapData[2]);
-			map = new int[bx][by];
-			for(int j=0;j<b;j++) {
-				String[] bData = sc.nextLine().split(" ");
-				int nx = Integer.parseInt(bData[0]);
-				int ny = Integer.parseInt(bData[1]);
-				map[nx][ny] = 1;
-			}
-			
-			boolean flag=false;
-			
-			do {
-				flag = false;
-				for(int x=0;x<bx;x++) {
-					for(int y=0;y<by;y++) {
-						int block = map[x][y];
-						if(block == 1)	{
-							check(x,y,bx,by);
-							flag = true;
-							answer++;
-							break;
-						}
-					}
-					if(flag) break;
-				}
-				
-			} while(flag);
-			
-			System.out.println(answer);
-		}
-		
-		sc.close();
-	}
-	
-	public static boolean check(int x, int y, int bx, int by) {
-		int[] dx = new int[] {0,0,1,-1};
-		int[] dy = new int[] {1,-1,0,0};
-		boolean result = true;
-		map[x][y] = 0;
-		for(int i=0;i<4;i++) {
-			int nx = x+dx[i];
-			int ny = y+dy[i];
-			if(nx<0 || ny<0 || ny==by || nx==bx)	continue;
-			if(map[nx][ny]==1) {
-				result = check(nx, ny, bx, by);
-			}
-		}
-		return result;
-	}
+            for(int i=0;i<m;i++) {
+                for(int j=0;j<n;j++) {
+                    if(map[i][j] && !isChecked[i][j]) {
+                        bfs(i, j);
+                        cnt++;
+                    }
+                }
+            }
 
+            sb.append(cnt).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static void bfs(int x, int y) {
+        int[] tmp;
+        q.offer(new int[] {x, y});
+        isChecked[x][y] = true;
+        while(!q.isEmpty()) {
+            tmp = q.poll();
+            for(int i=0;i<4;i++) {
+                nx = tmp[0]+dx[i];
+                ny = tmp[1]+dy[i];
+                if(nx>=0 && nx<m && ny>=0 && ny<n && map[nx][ny] && !isChecked[nx][ny]) {
+                    isChecked[nx][ny] = true;
+                    q.offer(new int[] {nx, ny});
+                }
+            }
+        }
+    }
 }
