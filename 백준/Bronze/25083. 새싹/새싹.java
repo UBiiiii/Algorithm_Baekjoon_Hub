447b1b18012bc9c59fe94ff86eeb8654a4208d2e
UBiiiii <96060708+UBiiiii@users.noreply.github.com>
2023-09-11 23:52:49 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb
+        .append("         ,r'\"7\n")
+        .append("r`-_   ,'  ,/\n")
+        .append(" \\. \". L_r'\n")
+        .append("   `~\\/\n")
+        .append("      |\n")
+        .append("      |");
+
+        System.out.println(sb);
+    }
+}
