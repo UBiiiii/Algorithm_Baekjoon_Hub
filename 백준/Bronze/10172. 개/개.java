2dad15cf9e66c46480ecb547c62360a8cedb1312
UBiiiii <96060708+UBiiiii@users.noreply.github.com>
2023-09-12 00:08:04 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 14020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb
+        .append("|\\_/|\n")
+        .append("|q p|   /}\n")
+        .append("( 0 )\"\"\"\\\n")
+        .append("|\"^\"`    |\n")
+        .append("||_/=\\\\__|\n");
+        System.out.println(sb.toString());
+    }
+}
