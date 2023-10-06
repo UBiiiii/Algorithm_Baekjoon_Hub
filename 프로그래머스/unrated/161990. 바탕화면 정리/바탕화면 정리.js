bb83d5111e9b849ff9fdc9eccf55d88b663ae839
UBiiiii <96060708+UBiiiii@users.noreply.github.com>
2023-10-06 14:49:19 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,12 +1,15 @@
-function solution(wallpaper) {
-    let y = [];
-    let x = [];
-    wallpaper.forEach((v, i)=>{
-        if(v.includes("#")) {
-            x.push(i);
-            y.push(v.indexOf("#"));
-            y.push(v.lastIndexOf("#"));
-        }  
-    })
-    return [Math.min(...x), Math.min(...y), Math.max(...x)+1, Math.max(...y)+1];
-}
+const solution = (wallpaper) => wallpaper.map((v, i)=>{
+        return [i, [...v].reduce((acc,curr,i)=>{
+            if(curr=='#')   acc.push(i);
+            return acc;
+        }, [])];
+    }).filter(el=>el[1].length>0).reduce((acc,curr)=>{
+        acc[0] = Math.min(curr[0], acc[0]);
+        acc[2] = Math.max(curr[0]+1, acc[2]);
+        curr[1].forEach(el=>{
+            acc[1] = Math.min(el, acc[1]);
+            acc[3] = Math.max(el+1, acc[3]);
+        })
+        
+        return acc;
+    }, [wallpaper.length, wallpaper[0].length, 0, 0])
